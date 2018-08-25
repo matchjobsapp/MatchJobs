03ca5c11001ce44e5f17c16af1e5c20978d19d81
Ivam Henrique Marques dos Santos <ivamks@gmail.com>
2018-08-25 20:04:44 -0300

    Primeira Tela (Splash) pronta.

--- app/src/main/java/io/github/matchjobsapp/matchjobs/Activity/MainActivity.java
+++ app/src/main/java/io/github/matchjobsapp/matchjobs/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package io.github.matchjobsapp.matchjobs;
+package io.github.matchjobsapp.matchjobs.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import io.github.matchjobsapp.matchjobs.R;
 
 public class MainActivity extends AppCompatActivity {
 
