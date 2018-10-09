4d899dfb010c561279df2587f1a7a1a6aca59e03
Ivam Santos <ivamks@gmail.com>
2018-10-09 19:00:07 -0300

    Inclusão tela login

--- app/src/main/java/io/github/matchjobsapp/matchjobs/Activity/MainActivity.java
+++ app/src/main/java/io/github/matchjobsapp/matchjobs/Activity/MainActivity.java
@@ -1,15 +1,30 @@
-package io.github.matchjobsapp.matchjobs.Activity;
+package io.github.matchjobsapp.matchjobs.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import io.github.matchjobsapp.matchjobs.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable{
+
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        handler.postDelayed(this, 3000);
+
+    }
+
+    @Override
+    public void run() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
