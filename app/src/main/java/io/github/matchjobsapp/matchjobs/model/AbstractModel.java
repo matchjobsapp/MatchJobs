d430cc7687c0a6b77ff5be88f1d45eb981d85ce9
Ivam Santos <ivamks@gmail.com>
2018-10-23 18:34:26 -0300

    Abstract Model

--- app/src/main/java/io/github/matchjobsapp/matchjobs/model/AbstractModel.java
+++ app/src/main/java/io/github/matchjobsapp/matchjobs/model/AbstractModel.java
@@ -13,7 +13,7 @@ import java.util.Objects;
  * │                                                    │
  * ╰────────────────────────────────────────────────────╯
  */
-public class AbstractModel<ID extends Serializable> implements Serializable {
+public abstract class AbstractModel<ID extends Serializable> implements Serializable {
 
     public ID getId() {
         return id;
