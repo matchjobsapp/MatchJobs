5207c8d4a43f7bcc9ea38ddf5620f305d58031f3
Ivam Santos <ivamks@gmail.com>
2018-10-26 18:56:29 -0300

    Correcao de erro de tela login

--- app/src/main/java/io/github/matchjobsapp/matchjobs/view/activity/LoginActivity.java
+++ app/src/main/java/io/github/matchjobsapp/matchjobs/view/activity/LoginActivity.java
@@ -1,5 +1,7 @@
 package io.github.matchjobsapp.matchjobs.view.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -14,4 +16,9 @@ import android.support.v7.app.AppCompatActivity;
  * ╰────────────────────────────────────────────────────╯
  */
 public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
