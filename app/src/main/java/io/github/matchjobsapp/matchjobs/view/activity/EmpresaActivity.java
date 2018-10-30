44a3fe509c04f76a6821a0ed2329c1dd327bd0b8
Victor Henrique <victor.toti@hotmail.com>
2018-10-29 21:08:39 -0300

    Classe de cadastro de empresa

--- app/src/main/java/io/github/matchjobsapp/matchjobs/view/activity/EmpresaActivity.java
+++ app/src/main/java/io/github/matchjobsapp/matchjobs/view/activity/EmpresaActivity.java
@@ -3,8 +3,12 @@ package io.github.matchjobsapp.matchjobs.view.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import io.github.matchjobsapp.matchjobs.R;
+import io.github.matchjobsapp.matchjobs.model.Empresa;
+
 /**
  * ╭────────────────────────────────────────────────────╮
  * │                                                    │
@@ -18,10 +22,25 @@ import io.github.matchjobsapp.matchjobs.R;
  */
 public class EmpresaActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
+
+        EditText etNomeEmpresa = findViewById(R.id.etNomeEmpresa);
+        EditText etEmailEmpresa = findViewById(R.id.etNomeEmpresa);
+        EditText etSenhaEmpresa = findViewById(R.id.etNomeEmpresa);
+
+        Button btSalvarEmpresa = findViewById(R.id.btSalvarEmpresa);
+        btSalvarEmpresa.setOnClickListener(view -> {
+            clickSalvar(
+                    etNomeEmpresa.getText().toString(),
+                    etEmailEmpresa.getText().toString(),
+                    etSenhaEmpresa.getText().toString()
+            );
+        } );
+
     }
 
     @Override
@@ -29,5 +48,14 @@ public class EmpresaActivity extends AppCompatActivity {
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
         finish();
+    }
+
+    private void clickSalvar(String nomeEmpresa, String email, String senha){
+        Empresa empresa = new Empresa();
+        empresa.setNomeEmpresa(nomeEmpresa);
+        empresa.setEmail(email);
+        empresa.setSenha(senha);
+
+        // TODO Service de salvar senha.
     }
 }
