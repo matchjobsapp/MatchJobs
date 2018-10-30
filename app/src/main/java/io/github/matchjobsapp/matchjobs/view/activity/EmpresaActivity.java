74c3a312a35f93b4275d689e857558b38445c695
Victor Henrique <victor.toti@hotmail.com>
2018-10-29 21:51:37 -0300

    Classe de cadastro de empresa

--- app/src/main/java/io/github/matchjobsapp/matchjobs/view/activity/EmpresaActivity.java
+++ app/src/main/java/io/github/matchjobsapp/matchjobs/view/activity/EmpresaActivity.java
@@ -1,10 +1,12 @@
 package io.github.matchjobsapp.matchjobs.view.activity;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import io.github.matchjobsapp.matchjobs.R;
 import io.github.matchjobsapp.matchjobs.model.Empresa;
@@ -23,14 +25,19 @@ import io.github.matchjobsapp.matchjobs.model.Empresa;
 public class EmpresaActivity extends AppCompatActivity {
 
 
+    private AlertDialog.Builder builder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
 
         EditText etNomeEmpresa = findViewById(R.id.etNomeEmpresa);
-        EditText etEmailEmpresa = findViewById(R.id.etNomeEmpresa);
-        EditText etSenhaEmpresa = findViewById(R.id.etNomeEmpresa);
+        EditText etCnpj = findViewById(R.id.etCnpj);
+        EditText etEndereco = findViewById(R.id.etEndereco);
+        EditText etTelefone = findViewById(R.id.etTelefone);
+        EditText etEmailEmpresa = findViewById(R.id.etEmailEmpresa);
+        EditText etSenhaEmpresa = findViewById(R.id.etSenhaEmpresa);
 
         Button btSalvarEmpresa = findViewById(R.id.btSalvarEmpresa);
         btSalvarEmpresa.setOnClickListener(view -> {
@@ -41,13 +48,21 @@ public class EmpresaActivity extends AppCompatActivity {
             );
         } );
 
+        builder = new AlertDialog.Builder(this, R.style.AppDialog);
+
     }
 
     @Override
     public void onBackPressed() {
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
-        finish();
+            builder.setTitle(getResources().getString(R.string.atencao));
+            builder.setMessage(getResources().getString(R.string.cancelarCadastro));
+            builder.setPositiveButton(getResources().getString(R.string.sim), (dialog, which) -> {
+                Intent i = new Intent(this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            });
+            builder.setNegativeButton(getResources().getString(R.string.nao), null);
+            builder.show();
     }
 
     private void clickSalvar(String nomeEmpresa, String email, String senha){
